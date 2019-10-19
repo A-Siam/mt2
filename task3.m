29a4a6039b7c50fcaea278e1b8a820514c8fecad
Abdullah khaled <34966791+A-Siam@users.noreply.github.com>
2019-10-19 19:56:57 +0200

    Update task3.m

--- task3.m
+++ task3.m
@@ -1,21 +1,27 @@
   warning("off")
 pkg load symbolic
-% no need to enter an or bn because there alue 
-function result = generate_sawtooth_no_for_loop(a0,T,time,n_ending)
-  syms n
-  result = 0.5*a0;
-  result = result + (-a0/pi)*symsum((-1).^n.*(1/n).*sin(2*n*pi*time/(T/2)),1,n_ending);
-  result = double(result);
-endfunction
+% this function is  deprecated as it is too slow 
+% --------------------DEPRECATED----------------------
+%function result = generate_sawtooth_no_for_loop(a0,T,time,n_ending)
+%  syms n
+%  result = 0.5*a0;
+%  result = result + (-a0/pi)*symsum((-1).^(n-1).*(1/n).*sin(2*n*pi*time/(T/2)),1,n_ending);
+%  result = double(result);
+%endfunction
+%
 
-function result = generate_sawtooth_with_for_loop(a0,T,time,n_ending)
-  result = 0.5*a0;
+% generate_sawtooth wave with amplutude of A using fourier series
+% using the previously known parameters source: 
+% no need to enter an or bn because there because there are already known
+function result = generate_sawtooth(A,T,time,n_ending)
+  result = 0.5;
   for n = 1:n_ending
-    result = result + (-a0/pi)*(-1).^n.*(1/n)*sin(2*n*time/(T/2));
+    result = result - (-1/pi)*(-1).^(n-1).*(1/n)*sin(2*n*time/(T/2));
   endfor
+  result = A* result
 endfunction
 
 
 x = -4:0.01:4;
-r = generate_sawtooth_with_for_loop(1,2,x,50);
+r = generate_sawtooth(2,2,x,100);
 plot(x,r);
